f252beaee46277213755322dd10fe6f7fee386f4
maopa <maopa@bk.ru>
2024-09-04 17:31:38 +0300

    Move function

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,16 @@
 fun main() {
     val amount = 5
 
-    fun tax(amount: Int): Double {
-        return if (amount * COMMISSION < MIN_TAX) {
-            MIN_TAX.toDouble()
-        } else {
-            amount * COMMISSION
-        }
-    }
     println(tax(amount))
 }
+
+fun tax(amount: Int): Double {
+    return if (amount * COMMISSION < MIN_TAX) {
+        MIN_TAX.toDouble()
+    } else {
+        amount * COMMISSION
+    }
+}
+
 const val COMMISSION = 0.75
 const val MIN_TAX = 35
